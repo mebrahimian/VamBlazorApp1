58f525aaf7319876881314c885f3cc715455c7e9
mebrahimian <ebrahimian_4585@hotmail.com>
2025-07-12 23:58:27 +0330

    change delay gst

--- Application/Dtos/PersonDto.cs
+++ Application/Dtos/PersonDto.cs
@@ -45,9 +45,6 @@ namespace VamBlazor.Client.Application.Dtos
         public string? HesabBank { get; set; }
         public string? V_CityDesc { get; set; } 
         public string? FullName { get; set; }   
-
-        public string CodePlaceHolder => "کدسپرده گذار را وارد کنید "; 
-        public string NamePlaceHolder => "نام را وارد کنید ";
-        public string FamilyPlaceHolder => "نام خانوادگی را وارد کنید ";
+                
     }
 }
